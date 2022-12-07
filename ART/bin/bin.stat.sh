5d45f32af542f15dc081a2f1c40f6d0efbec383e
Yuqi NIE <nieyuqi.cn@gmail.com>
2022-12-07 14:12:55 +0100

    status

---
+++ ART/bin/bin.stat.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# @File     :   bin.stat.sh
+# @Version  :   1.0.0
+# @Author   :   NIE Yuqi
+# @Email    :   nieyuqi.cn@gmail.com
+# @Time(CET):   2022/12/07 12:36:00
+#Description:
+	# 
+
+seqkit version >> info.software_version.txt
+
+seqkit stat -a ../input/hap/Ppy362_CCS3.HiFiasm.*.fasta.gz >> ../output/stat.txt
