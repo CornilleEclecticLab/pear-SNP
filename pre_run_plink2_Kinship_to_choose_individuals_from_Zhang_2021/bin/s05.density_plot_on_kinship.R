03468de1353d8d9b0aede4d97a72d4d7e4dd4bb4
Creeki <38589382+Creeki@users.noreply.github.com>
2022-12-15 16:35:14 +0100

    edit plot.R

--- pre_run_plink2_Kinship_to_choose_individuals_from_Zhang_2021/bin/s05.density_plot_on_kinship.R
+++ pre_run_plink2_Kinship_to_choose_individuals_from_Zhang_2021/bin/s05.density_plot_on_kinship.R
@@ -16,3 +16,39 @@ p <- ggplot() +
   geom_vline(aes(xintercept=0.354))
 p
 
+
+
+data2 <- read.table("../input_local/depth.kinship_cutoff0.125.txt",
+                   header = TRUE)
+head(data2)
+
+p <- ggplot() +
+  geom_histogram(data=data2, aes(x=depth), bins =40,  
+                 fill="lightcoral", color="black",alpha=0.5)+
+  ylab("Num of pear individuals from Zhang2021" )+
+  xlab("Depth  (times based on 540M bp)")+
+  geom_vline(aes(xintercept=10))+
+  theme_classic()+
+  scale_x_continuous(breaks=seq(5,16,1)) 
+p
+
+p+geom_density(data=data2, aes(x=depth)) 
+
+p
+
+
+
+data3 <- read.table("../input_local/depth.project.removeUnclearSpeciesPrunus.RemoveGroupLessThan8individuals.txt",
+                    header = TRUE)
+p <- ggplot()+
+  geom_histogram(data=data3, aes(x=Depth, colors="black",fill=Dataset))+
+  theme_classic()+
+  geom_density(alpha=0.4)
+  scale_x_continuous(breaks=seq(0,40,5)) 
+p
+
+p <- ggplot()+
+  geom_histogram(data=data3, aes(x=Depth, color=Group))+
+  theme_classic()+
+  scale_x_continuous(breaks=seq(0,40,5)) 
+p
