25f577a346af430029bedf31e0a9b700f8550a44
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-02-16 16:58:17 +0100

    edit for running

--- run_Splitstree/bin/s01.get_nei_distance_plink.sh
+++ run_Splitstree/bin/s01.get_nei_distance_plink.sh
@@ -6,7 +6,7 @@ module purge
 module load bioinfo/plink-v1.90b5.3
 
 
-WORKDIR="/home/zruilin/work/Peach/run_Splitstree"
-INPUT="$WORKDIR/input/Combine_Chr.geno20_maf005.anno.syno.thin8k"
+WORKDIR="/work/zruilin/pear/run_Splitstree"
+INPUT="/work/zruilin/pear/run_Population_genetic_filter/output/batch1.reduce3EupropeWildPear/pear.Combine_Chr.geno20_maf005.anno.syno.thin8k"
 
 plink -bfile $INPUT --allow-extra-chr --distance 1-ibs square
