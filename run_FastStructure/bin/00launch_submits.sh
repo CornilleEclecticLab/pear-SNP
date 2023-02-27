4e4c105f9590764d2e1bb3657ef4eebf5b50006b
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-02-27 14:32:34 +0100

    edit for running

--- run_FastStructure/bin/00launch_submits.sh
+++
@@ -1,8 +0,0 @@
-
-cd /work/zruilin/pear/run_FastStructure/output/batch6.only_Prifolia
-for rep in `seq 2 15`
-do
-        cd ./K$rep
-        ls ./ | xargs -I {} sbatch -c 1 --mem=1G {}
-        cd ..
-done
