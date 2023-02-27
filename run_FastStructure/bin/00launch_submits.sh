49a8937f5dbc37463ec8ebd11f4c1ba51ac66500
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-02-27 14:15:35 +0100

    edit for running

--- run_FastStructure/bin/00launch_submits.sh
+++ run_FastStructure/bin/00launch_submits.sh
@@ -1,5 +1,5 @@
 
-cd /work/zruilin/pear/run_FastStructure/output/batch2.removeCloneAnd3LowQualEuropeWildPear
+cd /work/zruilin/pear/run_FastStructure/output/batch6.only_Prifolia
 for rep in `seq 2 15`
 do
         cd ./K$rep
