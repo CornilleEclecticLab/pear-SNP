dbd28d7e62e8e486e49bfe23d5791faa2611a693
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-02-27 14:16:44 +0100

    FIX the PVE calculating

--- run_PCA_plink/bin/s01.get_pca_plink.sh
+++ run_PCA_plink/bin/s01.get_pca_plink.sh
@@ -9,15 +9,22 @@ module load bioinfo/plink-v1.90b5.3
 WORKDIR="/work/zruilin/pear/run_PCA_plink/"
 INPUT="/work/zruilin/pear/run_Population_genetic_filter/output/batch1.reduce3EupropeWildPear/pear.Combine_Chr.geno20_maf005.anno.syno.thin8k"
 OUTDIR="$WORKDIR/output/batch1.reduce3EupropeWildPear"
+set="pear.Combine_Chr.geno20_maf005.anno.syno.thin8k"
 
 mkdir -p $OUTDIR
 
 
-plink -bfile $INPUT --allow-extra-chr --pca
+plink -bfile $INPUT \
+    --allow-extra-chr \
+    --pca var-wts \
+    --make-rel \
+    --out ${set}.pca
 
-perl get_R_data.pl
+# perl get_R_data.pl
+perl bin.calculate_pve.pl $set
 
-mv ./plink.* $OUTDIR/
-mv ./*.eigenv* $OUTDIR/
-mv ./pca_input.data $OUTDIR/
 
+#mv ./plink.* $OUTDIR
+#mv ./*.eigenv* $OUTDIR
+#mv ./pca_input.data $OUTDIR
+#mv ./${set}.pac* $OUTDIR
