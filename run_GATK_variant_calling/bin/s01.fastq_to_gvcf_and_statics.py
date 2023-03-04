ff7d2d3de35db9f4b7b98a6f78cd7c1c6029f702
NIE Yuqi <nieyuqi.cn@gmail.com>
2023-03-04 12:51:54 +0100

    edit for running

--- run_GATK_variant_calling/bin/s01.fastq_to_gvcf_and_statics.py
+++ run_GATK_variant_calling/bin/s01.fastq_to_gvcf_and_statics.py
@@ -22,7 +22,7 @@ print("{0:=^80}".format(' Start '))
 # Set path
 user        = getpass.getuser()
 work_dir 	= '/work/'+user+'/pear/run_GATK_variant_calling'
-group       = 'pear.Wu2018_part2'
+group       = 'pear.Zhang2021_b2_p1'
 
 
 ## NO need to change >>
