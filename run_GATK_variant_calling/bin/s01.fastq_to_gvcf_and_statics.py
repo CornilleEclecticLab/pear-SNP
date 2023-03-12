48a9c8a2eab4679f7fa0202030ebf2d8f28045b0
NIE Yuqi <nieyuqi.cn@gmail.com>
2023-03-12 21:03:37 +0100

    record jobID, change to use 2 cpu from 4 to call SNP

--- run_GATK_variant_calling/bin/s01.fastq_to_gvcf_and_statics.py
+++ run_GATK_variant_calling/bin/s01.fastq_to_gvcf_and_statics.py
@@ -64,7 +64,14 @@ while run == '':
     if run != '1' and run !='2':
         print("Unrecognized input, please try again.\n")
         run = ''
-
+        
+        
+# Record the job_id
+# Record the job ID
+if run == "1":
+    job_id_txt = open(prefix+'job_ID.txt', 'w')
+    
+    
 # Read paths of fastq files
 with open(fastq_list,'r') as fo:
     
@@ -202,6 +209,8 @@ samtools index {output_dir}/{group}.{sample}.sorted.markdu.cram \\
         command = "sbatch -c 4 --mem=32G "+scripts_dir+"/"+sample_prefix+".sh"
         submit  = os.popen(command, 'r')
         job_id  = submit.read().strip().split()[-1]
+        dependent_job_id = job_id
+        job_id_txt.write(sample_prefix+'.sh' + '\t' + job_id + '\n')
         print("\n\nInformation: Dealing with "+ group +' '+ sample )
         print("\nInformation: A job has been submitted: \n\t"+command +'\n')
         
@@ -250,12 +259,14 @@ gatk --java-options "-Xmx8g" IndexFeatureFile \\
             
         ## Submit this script as a job
         if run == '1':
-            command = "sbatch -c 4 --mem=10G --dependency=afterok:"+job_id+' '+scripts_dir+"/"+sample_prefix_chr+".sh"
+            command = "sbatch -c 2 --mem=10G --dependency=afterok:"+dependent_job_id+' '+scripts_dir+"/"+sample_prefix_chr+".sh"
+            job_id = submit.read().strip().split()[-1]
+            job_id_txt.write(sample_prefix+'.sh' + '\t' + job_id + '\n')
             submit = os.popen(command, 'r')
             print("Information: A job has been submitted: \n\t"+command+'\n')
         
 
-
+job_id_txt.close()
 
 
 end_time = datetime.datetime.now()
