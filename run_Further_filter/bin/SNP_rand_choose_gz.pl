9d2d2ac50240afe5fcec50ed5fd7950cf4c95e17
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-03-14 16:48:50 +0100

    fix bug ghost # line

--- run_Further_filter/bin/SNP_rand_choose_gz.pl
+++ run_Further_filter/bin/SNP_rand_choose_gz.pl
@@ -5,27 +5,34 @@ use Data::Dumper;
 use File::Basename;
 use Getopt::Long;
 use List::Util qw(shuffle);
+
 # By Xilong CHEN
 # Create date: 2021-12-20
 # Contact: chen_xilong@outlook.com
 
+# update 2023-03-14
+
 my $usage = "perl $0 <window_size> <input_vcf.gz> <output_vcf>\n";
 
-my $win = shift @ARGV;
+my $win     = shift @ARGV;
 my $invcfgz = shift @ARGV;
-my $ouf = shift @ARGV;
+my $ouf     = shift @ARGV;
 
 open my $ou, ">", "$ouf";
 open my $z, '-|', '/usr/bin/gunzip', '-c', "$invcfgz";
 
 my @buffer;
 while (<$z>) {
-    print $ou $_ if /^#/;
-    push @buffer, $_;
-    if (@buffer == $win || eof) {
-        @buffer = shuffle(@buffer);
-        print $ou $buffer[0];
-        @buffer = ();
+    if ( $_ =~ /^#/ ) {
+        print $ou $_;
+    }
+    else {
+        push @buffer, $_;
+        if ( @buffer == $win || eof ) {
+            @buffer = shuffle(@buffer);
+            print $ou $buffer[0];
+            @buffer = ();
+        }
     }
 }
 
