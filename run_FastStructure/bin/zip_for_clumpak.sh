d53968f61527bb3829e26626756500715ce44d76
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-03-16 16:40:48 +0100

    new zip_for_clumpak.sh

---
+++ run_FastStructure/bin/zip_for_clumpak.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd /work/zruilin/pear/run_FastStructure/output/batch5.only_Communis_without_clone/
+mkdir result
+cp ./K*/*.meanQ ./result
+
+cd result
+for i in `seq 2 15`
+do
+    zip -q K${i}.zip *.${i}.meanQ
+done
+
+zip -q ../input_clumpak.zip K*.zip
+
+rm -rf K*.zip
