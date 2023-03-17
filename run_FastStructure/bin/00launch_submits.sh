705695adc99dac25191aec1098d808d0d91d9549
Yuqi NIE <nieyuqi.cn@gmail.com>
2023-03-17 18:11:17 +0100

    rename

--- run_FastStructure/bin/00launch_submits.sh
+++
@@ -1,8 +0,0 @@
-
-cd /work/zruilin/pear/run_FastStructure/output/batch3.removeLoquatColoneAnd3LowQualEuropeWildPear
-for rep in `seq 2 15`
-do
-        cd ./K$rep
-        ls ./ | xargs -I {} sbatch -c 1 --mem=1G {}
-        cd ..
-done
