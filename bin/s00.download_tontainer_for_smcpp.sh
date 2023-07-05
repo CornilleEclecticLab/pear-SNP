74ffddb927bbf1e2a916e931e900b76df12e8256
NIE Yuqi <5000-yuqi@users.noreply.forgemia.inra.fr>
2023-07-05 19:52:30 +0200

    new

---
+++ bin/s00.download_tontainer_for_smcpp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# @File     :   s00.download_tontainer_for_smcpp.sh
+# @Version  :   1.0.0
+# @Author   :   NIE Yuqi
+# @Email    :   nieyuqi.cn@gmail.com
+# @Time(CET):   2023/07/05 13:47:48
+#Description:
+	# 
+
+
+# load singularity modules
+module load system/singularity-3.7.3
+
+# download container
+singularity pull smcpp.sif docker://terhorst/smcpp:latest
