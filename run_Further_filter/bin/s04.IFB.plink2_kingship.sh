723ba08907b7df85fa84c304b4f405ca8c90781a
NIE Yuqi <nieyuqi.cn@gmail.com>
2023-07-30 22:24:34 +0200

    EFR and set variant for the file path

--- run_Further_filter/bin/s04.IFB.plink2_kingship.sh
+++ run_Further_filter/bin/s04.IFB.plink2_kingship.sh
@@ -11,34 +11,39 @@ module load plink2
 module load plink
 # PLINK v1.90b5 64-bit (14 Nov 2017)
 
-plink --vcf /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/whole_pear.combine.test_rand.vcf.gz \
+OUTPUT='/shared/ifbstor1/projects/pear_snp3/pear/run_Further_filter/output/pear_Jul2023.m2M2_maf005'
+PREFIX='pear_Jul2023.test_rand'
+
+mkdir -p $OUTPUT
+
+plink --vcf $OUTPUT/$PREFIX.vcf.gz \
 --make-bed \
 --const-fid \
---out /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/whole_pear.combine.test.rand \
+--out $OUTPUT/$PREFIX \
 --set-missing-var-ids @:# \
 --keep-allele-order \
 --allow-extra-chr
 
 
-mkdir -p /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/s04.clone_filtering
+mkdir -p $OUTPUT/s04.clone_filtering
 
 # Do not change --bfile to --vcf 
 	# If you only have vcf files, use the upwards to transform them into bfile format. 
 #Add --allow-extra-chr 
 	# Invalid chromosome code 'Pp01' on line 240 of --vcf file.
 	# (Use --allow-extra-chr to force it to be accepted.)
-plink2 --bfile /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/whole_pear.combine.test.rand \
+plink2 --bfile $OUTPUT/$PREFIX \
 --allow-extra-chr \
 --make-king-table \
 --king-cutoff 0.354 \
 --make-bed \
---out /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/s04.clone_filtering/without_clone0.354
+--out $OUTPUT/s04.clone_filtering/without_clone0.354
 
-plink2 --bfile /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/whole_pear.combine.test.rand \
+plink2 --bfile $OUTPUT/$PREFIX \
 --allow-extra-chr \
 --make-king-table \
 --king-cutoff 0.125 \
 --make-bed \
---out /shared/ifbstor1/projects/pear_snp2/pear/run_Further_filter/output/whole_pear/s04.clone_filtering/without_2nd-degree_relation0.125
+--out $OUTPUT/s04.clone_filtering/without_2nd-degree_relation0.125
 
 
