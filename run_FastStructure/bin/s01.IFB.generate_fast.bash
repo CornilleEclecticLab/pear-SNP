44a9051eec9d9efacae61af8d0f85aad46a2a42d
NIE Yuqi <nieyuqi.cn@gmail.com>
2023-08-03 11:05:05 +0200

    EFR, for IFB

---
+++ run_FastStructure/bin/s01.IFB.generate_fast.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+WORKDIR="/work/zruilin/pear/run_FastStructure"
+
+BED="/work/zruilin/pear/run_Population_genetic_filter/output/batch4.remove_b3_remove_Pashia_Pseudopshia/pear.Combine_Chr.geno20_maf005.anno.syno.thin8k"
+
+OUTDIR="$WORKDIR/output/batch4.remove_b3_Pashia_Pseudopshia"
+
+mkdir -p $OUTDIR
+
+perl ./mk_faststructure_IFB_cv.pl -b $BED -minK 2 -maxK 15 -r 30 -o $OUTDIR
+
--- run_Population_genetic_filter/bin/s05.IFB.filter_thin.sh
+++ run_Population_genetic_filter/bin/s05.IFB.filter_thin.sh
@@ -9,10 +9,10 @@ module purge
 module load samtools/1.14
 . ~/.bashrc
 
-WORKDIR="/shared/ifbstor1/projects/pear_snp2/pear/run_Population_genetic_filter"
+WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter"
 INPUT="$WORKDIR/input"
-OUTPUT="$WORKDIR/output/branch7.whole_pear"
-PREFIX="whole_pear"
+OUTPUT="$WORKDIR/output/branch8.batch15.pear_Jul2023_noclone"
+PREFIX="pear_Jul2023_noclone"
 
 conda activate vcftools-0.1.15
 vcftools --gzvcf $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.syno.vcf.gz --thin 8000 \
