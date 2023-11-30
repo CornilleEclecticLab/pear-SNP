bc6afa341e5d372b464445a0a9cb1ef69367b6a9
NIE Yuqi <nieyuqi.cn@gmail.com>
2023-11-30 13:21:43 +0100

    EFR

--- run_Population_genetic_filter/bin/s01.combine_chrvcf.sh
+++ run_Population_genetic_filter/bin/s01.combine_chrvcf.sh
@@ -7,17 +7,18 @@
 #SBATCH -J s01.combine_chrvcf
 #SBATCH -o s01.combine_chrvcf.NOGIT.%J.out
 #SBATCH -e s01.combine_chrvcf.NOGIT.%J.err
+#SBATCH -c 4
 
 module purge
 module load bcftools/1.14
 
 WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter"
 INPUT="$WORKDIR/input"
-OUTPUT="$WORKDIR/output/branch8.pear_Jul2023"
+OUTPUT="$WORKDIR/output/branch15.pear_Oct2023"
 
 mkdir -p $OUTPUT
 
-bcftools concat -f $INPUT/pear_Jul2023.chr.variant.vcf.list \
+bcftools concat -f $INPUT/pear_Oct2023.chr.variant.vcf.list \
 -O z4 \
 --threads 8 \
--o $OUTPUT/pear_Jul2023.Combine_Chr.vcf.gz
+-o $OUTPUT/pear_Oct2023.Combine_Chr.vcf.gz
