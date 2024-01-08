d00d2c65d37beff560f82c84c1ca827e1a961382
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-08 18:19:36 +0100

    EFR and new config file

--- run_Population_genetic_filter/bin/s03.IFB.snpEff_annotaion.sh
+++ run_Population_genetic_filter/bin/s03.IFB.snpEff_annotaion.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
 #SBATCH -J s03.snpEff_annotaion
-#SBATCH -o s03.snpEff_annotaion.%j.out
-#SBATCH -e s03.snpEff_annotaion.%j.err
+#SBATCH -o s03.snpEff_annotaion.NOGIT.%j.out
+#SBATCH -e s03.snpEff_annotaion.NOGIT.%j.err
 
 module purge
 module load bcftools/1.14
 module load samtools/1.14
 module load java-jdk/11.0.9.1
+source s00.config.sh
 
-
-WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter"
-INPUT="$WORKDIR/input"
-OUTPUT="$WORKDIR/output/branch8.batch15.pear_Jul2023_noclone"
-PREFIX="pear_Jul2023_noclone"
-
-tabix -p vcf $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.vcf.gz
+tabix -p vcf $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.vcf.gz
 
 java -Xmx8g -jar $WORKDIR/bin/snpEff/snpEff.jar Pyr.cuiguan \
-$OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.vcf.gz > \
-$OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.vcf
+$OUTPUT/$PREFIX.Combine_chr.geno20_maf005.vcf.gz \
+    > $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.anno.vcf
 
 mv snpEff_* $OUTPUT/
 
-bgzip $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.vcf && \
-tabix -p vcf $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.vcf.gz
+bgzip $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.anno.vcf && \
+tabix -p vcf $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.anno.vcf.gz
--- run_Population_genetic_filter/bin/s06.IFB.plink_vcf2bed.sh
+++ run_Population_genetic_filter/bin/s06.IFB.plink_vcf2bed.sh
@@ -3,20 +3,17 @@
 # 9/11/2022
 
 #SBATCH -J s06.plink_vcf2bed
-#SBATCH -o s06.plink_vcf2bed.%j.out
-#SBATCH -e s06.plink_vcf2bed.%j.err
+#SBATCH -o s06.plink_vcf2bed.NOGIT.%j.out
+#SBATCH -e s06.plink_vcf2bed.NOGIT.%j.err
 module purge
 module load plink
 
-WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter"
-INPUT="$WORKDIR/input"
-OUTPUT="$WORKDIR/output/branch8.batch15.pear_Jul2023_noclone"
-PREFIX="pear_Jul2023_noclone"
+source s00.config.sh
 
-plink --vcf $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.syno.thin8k.vcf.gz \
+plink --vcf $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.anno.syno.thin8k.vcf.gz \
 --make-bed \
 --const-fid \
---out $OUTPUT/$PREFIX.Combine_Chr.geno20_maf005.anno.syno.thin8k \
+--out $OUTPUT/$PREFIX.Combine_chr.geno20_maf005.anno.syno.thin8k \
 --set-missing-var-ids \@\:# \
 --keep-allele-order \
 --allow-extra-chr
