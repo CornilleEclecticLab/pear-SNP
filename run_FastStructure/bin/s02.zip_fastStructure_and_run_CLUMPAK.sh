6c4eb0e359d9f5118629bb823da04338e4c217dd
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-09 14:47:39 +0100

    unset LC_ALL=C, change output with random ID

--- run_FastStructure/bin/s02.zip_fastStructure_and_run_CLUMPAK.sh
+++ run_FastStructure/bin/s02.zip_fastStructure_and_run_CLUMPAK.sh
@@ -4,7 +4,7 @@
 #SBATCH -o s02.zip_fastStructure_and_run_CLUMPAK.NOGIT.%J.out
 #SBATCH -e s02.zip_fastStructure_and_run_CLUMPAK.NOGIT.%J.err
 #SBATCH -c 1
-#SBATCH --mem=16G
+#SBATCH --mem=2G
 
 # @File     :   s02.zip_fastStructure_and_run_CLUMPAK.sh
 # @Version  :   1.0.0
@@ -14,15 +14,24 @@
 #Description:
 
 
-
+####################
 # load config file
+####################
 source s00.config.sh
 
 
 ####################
 # zip FastStructure output
 ####################
+random_id=${RANDOM}
+
 cd ${OUTPUT_DIR}
+
+INPUT_CLUMPAK_ZIP="input_clumpak_${random_id}.zip"
+
+if [ -f $INPUT_CLUMPAK_ZIP ]; then
+    rm $INPUT_CLUMPAK_ZIP
+fi
 
 mkdir result && cd result
 
@@ -32,7 +41,7 @@ for i in `seq $MIN_K $MAX_K`
     do zip -q K${i}.zip *.${i}.meanQ
     done
 
-zip -q ../input_clumpak.zip K*.zip
+zip -q ../${INPUT_CLUMPAK_ZIP} K*.zip
 
 rm K*.zip
 rm *.meanQ
@@ -49,26 +58,24 @@ ${LOAD_PERL5}
 
 cd ${CLUMPAK_DIR}
 
-OUTPUT_CLUMPAK="./output_clumpak"
+# This is unnecessary for on IFB cluster
+# export LC_ALL=C
 
-if [ -d "${OUTPUT_CLUMPAK}" ]; then
-    rm -r "${OUTPUT_CLUMPAK}"
-fi
-
-#Xilong: If you are using the anther language, the CLUMPAK may have some bug, LC_ALL=C is changing the language to C(C for computer)
-export LC_ALL=C
-
-random_id=${RANDOM}
 
 if [ -d "tmp_clumpak_${random_id}" ]; then
     rm -r "tmp_clumpak_${random_id}"
+fi
+
+OUTPUT_CLUMPAK="./output_clumpak_${random_id}"
+
+if [ -d "${OUTPUT_CLUMPAK}" ]; then
+    rm -r "${OUTPUT_CLUMPAK}"
 fi
 
 # NOTE: "./" in the --dir paramater is necessary
 ./CLUMPAK.pl \
     --id "tmp_clumpak_${random_id}" \
     --dir "${OUTPUT_CLUMPAK}" \
-    --file "${OUTPUT_DIR}/input_clumpak.zip" \
+    --file "${OUTPUT_DIR}/${INPUT_CLUMPAK_ZIP}" \
     --inputtype admixture \
-&& \
-mv "${OUTPUT_CLUMPAK}" "${OUTPUT_DIR}/"
+&& mv "${OUTPUT_CLUMPAK}" "${OUTPUT_DIR}/" 
