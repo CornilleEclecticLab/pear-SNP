6e3b51c61b891b8c89500948ca0d4f93c828b664
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-10 10:57:00 +0100

    EFR

--- run_Population_genetic_filter/bin/s00.config.sh
+++ run_Population_genetic_filter/bin/s00.config.sh
@@ -1,4 +1,8 @@
 WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter"
 INPUT="${WORKDIR}/input"
-OUTPUT="${WORKDIR}/output/branch15.batch16.pear_Dec2023_noclone"
-PREFIX="pear_Dec2023_noclone"
+OUTPUT="${WORKDIR}/output/branch15.batch18.pear_Dec2023_noclone_no34Minority_no8conflict"
+PREFIX="pear_Dec2023_noclone_no34Minority_no8conflict"
+
+
+IN_OUTPUT="$WORKDIR/../run_Further_filter/output/s04.pear_Dec2023.variant_noAC0_norACeqAN_noHighMissingSample"
+IN_PREFIX="pear_Dec2023"
