63b81be6931c5b9934ea2a5abf02d3f31d312c46
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-10 15:43:12 +0100

    using s00.config.sh

---
+++ run_PCA_plink/bin/s00.config.sh
@@ -0,0 +1,4 @@
+WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_PCA_plink/"
+set="pear_Dec2023_noclone.Combine_chr.geno20_maf005.anno.syno.thin8k"
+OUTDIR="$WORKDIR/output/branch15.batch16.pear_Dec2023_noclone"
+INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch16.pear_Dec2023_noclone/${set}"
