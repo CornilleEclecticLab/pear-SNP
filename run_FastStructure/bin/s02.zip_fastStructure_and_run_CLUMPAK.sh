f99d1166e723a8c94fdf61034691c7088ad0d702
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-10 10:54:18 +0100

    load

--- run_FastStructure/bin/s02.zip_fastStructure_and_run_CLUMPAK.sh
+++ run_FastStructure/bin/s02.zip_fastStructure_and_run_CLUMPAK.sh
@@ -15,66 +15,72 @@
 
 
 ####################
-# load config file
+# Load config file
 ####################
 source s00.config.sh
 
 
 ####################
-# zip FastStructure output
+# Zip FastStructure output
 ####################
+cd "${OUTPUT_DIR}" || exit 1
+
 random_id=${RANDOM}
-
-cd ${OUTPUT_DIR}
-
 INPUT_CLUMPAK_ZIP="input_clumpak_${random_id}.zip"
 
-if [ -f $INPUT_CLUMPAK_ZIP ]; then
-    rm $INPUT_CLUMPAK_ZIP
+while [ -f "$INPUT_CLUMPAK_ZIP" ]; do
+    random_id=${RANDOM}
+    INPUT_CLUMPAK_ZIP="input_clumpak_${random_id}.zip"
+done
+
+TEMP_RESULT_DIR="temp_result_${random_id}"
+
+if [ -d "$TEMP_RESULT_DIR" ]; then
+    rm -r "$TEMP_RESULT_DIR" || exit 1 # exit with error code 1
 fi
 
-mkdir result && cd result
+mkdir "${TEMP_RESULT_DIR}" && cd "${TEMP_RESULT_DIR}"
 
 cp ../K*/*.meanQ .
 
-for i in `seq $MIN_K $MAX_K`
-    do zip -q K${i}.zip *.${i}.meanQ
-    done
+for i in $(seq "$MIN_K" "$MAX_K"); do
+    zip -q "K${i}.zip" *.${i}.meanQ
+done
 
-zip -q ../${INPUT_CLUMPAK_ZIP} K*.zip
+zip -q "../${INPUT_CLUMPAK_ZIP}" K*.zip
 
 rm K*.zip
 rm *.meanQ
 
-cd ${OUTPUT_DIR}
-rmdir ./result
+cd ${OUTPUT_DIR} || exit 1
+rmdir "${TEMP_RESULT_DIR}"
 
 
 ####################
 # run CLUMPAK
 ####################
-# load perl5
+# Load perl5 and ghostscript
 ${LOAD_PERL5}
+${LOAD_GHOSTSCRIPT}
 
-cd ${CLUMPAK_DIR}
+cd "${CLUMPAK_DIR}" || exit 1
 
 # This is unnecessary for on IFB cluster
 # export LC_ALL=C
 
-
-if [ -d "tmp_clumpak_${random_id}" ]; then
-    rm -r "tmp_clumpak_${random_id}"
+CLUMPAK_JOB="clumpak_${random_id}"
+if [ -d "$CLUMPAK_JOB" ]; then
+    rm -r "$CLUMPAK_JOB" || exit 1
 fi
 
 OUTPUT_CLUMPAK="./output_clumpak_${random_id}"
-
 if [ -d "${OUTPUT_CLUMPAK}" ]; then
-    rm -r "${OUTPUT_CLUMPAK}"
+    rm -r "${OUTPUT_CLUMPAK}" || exit 1
 fi
 
 # NOTE: "./" in the --dir paramater is necessary
 ./CLUMPAK.pl \
-    --id "tmp_clumpak_${random_id}" \
+    --id "${CLUMPAK_JOB}" \
     --dir "${OUTPUT_CLUMPAK}" \
     --file "${OUTPUT_DIR}/${INPUT_CLUMPAK_ZIP}" \
     --inputtype admixture \
