f9cd516e5f68e494f44fdc04bbc3898c1c24b2f4
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-12 17:45:32 +0100

    format plink output

--- run_Splitstree/bin/s01.IFB.get_nei_distance_plink.sh
+++ run_Splitstree/bin/s01.IFB.get_nei_distance_plink.sh
@@ -5,8 +5,8 @@
 module purge
 module load plink/1.90b6.18
 
+source s00.config.sh
 
-WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_Splitstree/bin"
-INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch8.batch15.pear_Jul2023_noclone/pear_Jul2023_noclone.Combine_Chr.geno20_maf005.anno.syno.thin8k"
+plink -bfile "$INPUT" --allow-extra-chr --distance 1-ibs square
 
-plink -bfile $INPUT --allow-extra-chr --distance 1-ibs square
+python3 s01.x1.format_mdist.py
