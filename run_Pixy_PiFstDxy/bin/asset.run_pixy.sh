28f8fcd13837d37143bd6f3ed99fdff475533bde
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-01-24 13:33:55 +0100

    nothing

--- run_Pixy_PiFstDxy/bin/asset.run_pixy.sh
+++ run_Pixy_PiFstDxy/bin/asset.run_pixy.sh
@@ -5,27 +5,14 @@
 # @Author   :   NIE Yuqi
 # @Email    :   nieyuqi.cn@gmail.com
 # @Time(CET):   2023/05/04 22:53:00
-#Description:
+# Description:
 	# 
+# Update: 2024-01-17 12:27:20
+# ChangeLog:
+    # 1. using source activate to load pixy conda environment
 
 
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/local/bioinfo/src/Miniconda/Miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/usr/local/bioinfo/src/Miniconda/Miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/usr/local/bioinfo/src/Miniconda/Miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/usr/local/bioinfo/src/Miniconda/Miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-
-conda activate pixy
+source activate pixy
 
 pixy --stats pi fst dxy \
 --vcf test.vcf.gz \
@@ -34,6 +21,6 @@ pixy --stats pi fst dxy \
 --n_cores 4 \
 --output_folder output \
 --output_prefix pixy_output \
---bypass_invariant_check yes
+--bypass_invariant_check no # Bypass the check for invariant sites. Use with caution!
 
 
