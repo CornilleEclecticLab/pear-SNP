1dc5a6c58bd835a6cd26a6d1bb90a01fb0ef6a9f
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-03-12 16:22:49 +0100

    EFR

--- run_FastStructure/bin/s00.config.sh
+++ run_FastStructure/bin/s00.config.sh
@@ -1,6 +1,5 @@
-# All the samples
+##### All the samples
 # s01 have been run
-# 
 # BATCH="branch15.batch21.pear_Dec2023.noclone"
 # BASE_PREFIX="pear_Dec2023.noclone.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
@@ -9,7 +8,13 @@
 # BATCH="branch15.batch24.pear_Dec2023.noClone"
 # BASE_PREFIX="pear_Dec2023.noClone.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
-# Estern samples
+# s01 run
+# s02 has been run
+# BATCH="branch15.batch30.noclone.removeConflictss"
+# BASE_PREFIX="pear_Dec2023.noClone.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
+
+##### Estern samples
 # s01 run
 # s02 run
 # BATCH="branch15.batch22.pear_Dec2023.Asia"
@@ -20,17 +25,42 @@
 # BATCH="branch15.batch25.pear_Dec2023.Asia"
 # BASE_PREFIX="pear_Dec2023.Asia.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
+# s01 has been run
+# s02 has been run
+# BATCH="branch15.batch26.pear_Dec2023.Asia"
+# BASE_PREFIX="pear_Dec2023.Asia.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
-# Western samples
+# s01 has been run
+# s02 has been run
+# BATCH="branch15.batch31.Asia.removeConflicts"
+# BASE_PREFIX="pear_Dec2023.Asia.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
+##### Western samples
 # s01 have been run
 # s02 run
-BATCH="branch15.batch23.pear_Dec2023.Europe"
-BASE_PREFIX="pear_Dec2023.Europe.Combine_chr.geno20_maf005.anno.syno.thin8k"
+# BATCH="branch15.batch23.pear_Dec2023.Europe"
+# BASE_PREFIX="pear_Dec2023.Europe.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
+# s01 have been run
+# s02 have been run
+# BATCH="branch15.batch27.pear_Dec2023.Europe"
+# BASE_PREFIX="pear_Dec2023.Europe.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
 
+# BATCH="branch15.batch18.pear_Dec2023_noclone_no34Minority_no8conflict"
+# BASE_PREFIX="pear_Dec2023_noclone_no34Minority_no8conflict.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
-#BATCH="branch15.batch18.pear_Dec2023_noclone_no34Minority_no8conflict"
-#BASE_PREFIX="pear_Dec2023_noclone_no34Minority_no8conflict.Combine_chr.geno20_maf005.anno.syno.thin8k"
+# Cultivar
+# s01 have been run
+# BATCH="branch15.batch28.Cultivar"
+# BASE_PREFIX="pear_Dec2023.Cultivar.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
+# Wild
+# s01 have been run
+# s02 have been run
+# BATCH="branch15.batch29.Wild"
+# BASE_PREFIX="pear_Dec2023.Wild.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
 
 PROJECT_DIR="/shared/ifbstor1/projects/pear_snp3/pear"
 
@@ -52,6 +82,8 @@ MAX_K=16
 LOAD_GHOSTSCRIPT='source activate ghostscript'
 
 LOAD_PERL5="module load perl/5.26.2"
+
+LOAD_R="module load r/4.2.3"
 
 CLUMPAK_DIR="${WORK_DIR}/bin/CLUMPAK.NOGIT./26_03_2015_CLUMPAK/CLUMPAK"
 
