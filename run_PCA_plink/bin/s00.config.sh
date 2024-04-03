f9f45d92263b89263a9b2c48b0ca3482652ff980
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-04-03 15:04:23 +0200

    Update Readme about PVE. EFR

--- run_PCA_plink/bin/s00.config.sh
+++ run_PCA_plink/bin/s00.config.sh
@@ -1,9 +1,9 @@
 WORKDIR="/shared/ifbstor1/projects/pear_snp3/pear/run_PCA_plink/"
 
-branch="branch15.batch25.pear_Dec2023.Asia"
-set="pear_Dec2023.Asia.Combine_chr.geno20_maf005.anno.syno.thin8k"
+LOAD_PLINK="module load plink/1.90b6.18"
 
-
+branch="branch15.batch37.Cultivar_pyri_CN"
+set="pear_Dec2023.Cultivar_pyri_CN.Combine_chr.geno20_maf005.anno.syno.thin8k"
 
 OUTDIR="$WORKDIR/output/${branch}"
 INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/${branch}/${set}"
--- run_PCA_plink/bin/s01.IFB.get_pca_plink.sh
+++ run_PCA_plink/bin/s01.IFB.get_pca_plink.sh
@@ -2,22 +2,23 @@
 #SBATCH -J s01.get_pca_plink
 #SBATCH -o s01.get_pca_plink.%J.out
 #SBATCH -e s01.get_pca_plink.%J.err
-module purge
-module load plink/1.90b6.18
 
+module purge
 source s00.config.sh
+${LOAD_PLINK}
 
 mkdir -p "$OUTDIR"
 
-
-plink -bfile "${INPUT}" \
+plink --bfile "${INPUT}" \
     --allow-extra-chr \
     --pca var-wts \
     --make-rel \
     --out "${OUTDIR}/${set}.pca"
 
 cd "$OUTDIR" || exit 1
-# perl get_R_data.pl # Disused, as Plink couldn't calculate the Proportion of variance explained(pve) directly, but this script set eigenvalues as PVE. To calculate PVE, use following new script.
+
+# perl get_R_data.pl # Disused, as Plink couldn't calculate the 
+                     # Proportion of variance explained(pve) directly, 
+                     # but this script set eigenvalues as PVE. 
+                     # To calculate PVE, use following new script.
 perl "${WORKDIR}/bin/bin.calculate_pve.pl" "$set"
-
-
