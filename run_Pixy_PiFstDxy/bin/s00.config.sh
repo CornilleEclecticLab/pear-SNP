7071764301ae46f9dd1728d82b6d3630c62f85dd
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-04-11 15:10:57 +0200

    update annotation

--- run_Pixy_PiFstDxy/bin/s00.config.sh
+++ run_Pixy_PiFstDxy/bin/s00.config.sh
@@ -2,7 +2,8 @@
 LOAD_PIXY="module load pixy/1.2.7.beta1"
 LOAD_BCFTOOLS="module load bcftools/1.14"
 
-# The following sets only effects in the s01.run_pixy.sh script
+# The following sets only effect in the s01.run_pixy.sh script for testing,
+# not in s02.run_pixy_by_chromosome.py
 INPUT_VCF="test.vcf.gz"
 BIN_DIR=$(pwd)
 INPUT_DIR="${BIN_DIR}/../input"
