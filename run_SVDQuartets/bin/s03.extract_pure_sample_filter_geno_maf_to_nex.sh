516bf94cd96671a62570493a7a517b7cd15950a0
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-04-17 16:31:14 +0200

    load python3

--- run_SVDQuartets/bin/s03.extract_pure_sample_filter_geno_maf_to_nex.sh
+++ run_SVDQuartets/bin/s03.extract_pure_sample_filter_geno_maf_to_nex.sh
@@ -10,6 +10,7 @@
 #SBATCH -e s03.extract_pure_sample_filter_geno_maf_to_nex.%j.err
 module purge
 module load bcftools/1.14
+module load python
 
 # Define the number of threads for parallel processing
 THREADS=8
@@ -52,16 +53,16 @@ bcftools view "$INPUT_VCF" \
 VCF2PHYLIP="vcf2phylip.py"
 
 if [ ! -e "$VCF2PHYLIP" ]; then
-	wget https://raw.githubusercontent.com/edgardomortiz/vcf2phylip/master/vcf2phylip.py
+    wget https://raw.githubusercontent.com/edgardomortiz/vcf2phylip/master/vcf2phylip.py
 fi
 
 
 # Convert VCF to NEX
-/usr/bin/python3 vcf2phylip.py \
-	--input $OUTPUT_VCF \
+python3 vcf2phylip.py \
+    --input $OUTPUT_VCF \
     --output-folder $OUTPUT_DIR \
     --phylip-disable \
-	--nexus
+    --nexus
 
 
 # Combine nexus files
