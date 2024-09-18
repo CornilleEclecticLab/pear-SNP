97311c9aaab03febd1da0af9f9cf916533839777
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-09-18 15:59:31 +0200

    SweeD scripts

---
+++ run_SweeD_refer_on_communis/bin/s00.load_SweeD.sh
@@ -0,0 +1,1 @@
+PATH=/shared/ifbstor1/projects/pear_snp2/software/sweed:$PATH
---
+++ run_SweeD_refer_on_communis/bin/s00_config.py
@@ -0,0 +1,6 @@
+batch='pear_Jul2024'
+input_merged_vcf_path = '/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter_refer_on_communis/output/pear_Jul2024_ref_comm.branch17.whole_pear/pear_Jul2024_ref_comm.Combine_chr.vcf.gz'
+
+variant_vcf_list_filename = 's01.variant_vcf_list.txt'
+chromosomes_list_filename = 's01.chr_list.txt'
+individual_pop_map_filename = 's01.sample_tab_population.txt'
