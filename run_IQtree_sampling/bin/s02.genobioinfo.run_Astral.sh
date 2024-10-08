0bf79a6028cc82a595b2e7efe0b8d15e03fc64c7
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-10-08 09:53:09 +0200

    for genobioinfo

---
+++ run_IQtree_sampling/bin/s02.genobioinfo.run_Astral.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#SBATCH -J s02.run_Astral.sh
+#SBATCH -o s02.run_Astral.sh.%J.out
+#SBATCH -e s02.run_Astral.sh.%J.err
+#SBATCH -c 1
+
+# @File     :   s02.run_Astral.sh
+# @Version  :   1.0.0
+# @Author   :   NIE Yuqi
+# @Email    :   nieyuqi.cn@gmail.com
+# @Time(CET):   2024/10/04 10:35:53
+#Description:
+    # 
+
+module load devel/java/17.0.6
+
+mkdir -p ../output/s02.run_Astral
+
+cat ../output/s01.genobioinfo.sampling_and_run_IQtree/*.treefile \
+    > ../output/s02.run_Astral/concatenated.treefile
+
+ls ../output/s01.genobioinfo.sampling_and_run_IQtree/*.ufboot \
+    > ../output/s02.run_Astral/ufboot.list
+
+java -jar Astral.NOGIT./astral.5.7.8.jar \
+    -i ../output/s02.run_Astral/concatenated.treefile \
+    -b ../output/s02.run_Astral/ufboot.list \
+    -o ../output/s02.run_Astral/astral.species_boot.tree
+
+tail -n 1 ../output/s02.run_Astral/astral.species_boot.tree \
+    > ../output/s02.run_Astral/astral.species.tree
