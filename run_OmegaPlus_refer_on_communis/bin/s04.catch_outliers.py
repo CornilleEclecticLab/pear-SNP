1048bc2f5203164d1a0945a8c27c738551c20744
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-10-09 19:58:08 +0200

    update

--- run_OmegaPlus_refer_on_communis/bin/s04.catch_outliers.py
+++ run_OmegaPlus_refer_on_communis/bin/s04.catch_outliers.py
@@ -14,6 +14,9 @@
 # Update: v2.0.0 2024-09-29 21:54:15
 #    1. Get candidate genes from the outliers.
 
+# Update: v2.1.0 2024-10-08 15:02:42
+#    1. Use more cutoffs for outliers.
+
 import datetime
 import sys
 import textwrap
@@ -24,7 +27,7 @@ print(f'{" Start ":=^79}')
 
 
 
-version = "2.0.0"
+version = "2.1.0"
 script_basename = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 bin_dir = script_path
@@ -74,10 +77,12 @@ for pop in populations:
             raise ValueError(f'Unexpected format in {cutoff_file}')
 
 
+# Read outputs from OmegaPlus, and catch the outliers based on neutral envlope
 for pop in populations:
     # open output file
     output_file = open(os.path.join(
         sub_output_dir, f'OmegaPlus.outliers.{pop}.bed'), 'w')
+    results_pop = []
     for chr in chromosomes:
         outliers = []
         # Read the OmegaPlus output
@@ -86,58 +91,120 @@ for pop in populations:
             lines = f.readlines()
             for line in lines:
                 line = line.strip()
-                if line == '':
-                    continue
-                elif line.startswith('//'):
+                if line == '' or line.startswith('//'):
                     continue
                 else:
                     try:
                         position, omega, start, end, valid = line.split()
                     except ValueError:
                         raise ValueError(f'Unexpected format in {omega_plus_output_file}\n{line}')
+                    
                     omega = float(omega)
-                    start = int(float(start))-1
+                    start = int(float(start))-1 # convert to 0-based
                     end = int(float(end))
                     valid = bool(valid)
+                    
                     if valid:
-                        if omega >= cutoff_pop[pop]:
+                        # if (chr, start, end, omega) not in results:
+                            # results.append((chr, start, end, omega))
+                        results_pop.append((chr, start, end, omega)) # Collect all valid results
+                        if omega >= cutoff_pop[pop]:  # Check against the cutoff
                             outliers.append((chr, start, end))
                     else:
                         if omega >= cutoff_pop[pop]:
                             print(f'Warning: invalid outlier at {chr}:{position}\t{start}-{end}')
-            # sort the outliers by start position
-            outliers.sort(key=lambda x: x[1])    # Note: should sort chromosome first, sort -k1,1 -k2,2n
-            for chr, start, end in outliers:
-                output_file.write(f'{chr}\t{start}\t{end}\n')
+        # Remove duplicates
+        outliers = list(set(outliers))   # remove duplicates
+        
+        # Sort the outliers by start position within this chromosome
+        outliers.sort(key=lambda x: x[1])    # Note: should sort chromosome first, sort -k1,1 -k2,2n
+        for (chr, start, end) in outliers:
+            output_file.write(f'{chr}\t{start}\t{end}\n')
     output_file.close()
+    
+    # Remove duplicates from results
+    results_pop = list(set(results_pop))
+    
+    # Sort the results by omega
+    results_pop.sort(key=lambda x: x[3], reverse=True)
+    top_cut_off = [0.001, 0.01, 200, 500]
+    for top in top_cut_off:
+        if top < 1:
+            tops = max(1,int(float(top * len(results_pop))))
+        else:
+            tops = int(top)
+
+        # Write the top outliers
+        tops_outliers = results_pop[:tops]
+        # Sort the outliers by chromosome and by start position
+        tops_outliers.sort(key=lambda x: (x[0], x[1]))
+        with open(os.path.join(sub_output_dir, f'OmegaPlus.{top}outliers.{pop}.bed'), 'w') as f:
+            for (chr, start, end, omega) in tops_outliers:
+                f.write(f'{chr}\t{start}\t{end}\n')
 
 
 # Merge the outliers
-merge_script = os.path.join(sub_script_dir, f'{script_basename}.merge.sh')
-with open(merge_script,'w') as f:
-    f.write(f"#!/bin/bash\n")
-    f.write(f"#SBATCH -e {os.path.basename(merge_script)}.%J.err\n")
-    f.write(f"#SBATCH -e {os.path.basename(merge_script)}.%J.out\n")
-    f.write(f"{load_bedtools}\n\n\n")
-    for pop in populations:
-        input_file = os.path.join(sub_output_dir, f'OmegaPlus.outliers.{pop}.bed')
-        output_file = os.path.join(sub_output_dir, f'OmegaPlus.outliers.{pop}.merged.bed')
-        gff_file = os.path.join(input_dir,gff_filename)
-        f.write(f"bedtools merge -i {input_file} > {output_file}\n")
+def merge_outliers(method, cutoff):
+    # cutoff likes '', '0.001', '0.01' or '500'
+    global sub_script_dir, script_basename, populations, load_bedtools,input_dir,sub_output_dir,gff_filename
+    merge_script = os.path.join(sub_script_dir, f'{script_basename}.{cutoff}merge.sh')
+    with open(merge_script,'w') as f:
+        f.write(f"#!/bin/bash\n")
+        f.write(f"#SBATCH -e {os.path.basename(merge_script)}.%J.err\n")
+        f.write(f"#SBATCH -o {os.path.basename(merge_script)}.%J.out\n")
+        f.write(f"{load_bedtools}\n\n\n")
+        for pop in populations:
+            input_file = os.path.join(sub_output_dir, f'{method}.{cutoff}outliers.{pop}.bed')
+            output_file = os.path.join(sub_output_dir, f'{method}.{cutoff}outliers.{pop}.merged.bed')
+            gff_file = os.path.join(input_dir,gff_filename)
+            f.write(f"bedtools merge -i {input_file} > {output_file}\n")
 
-        # Get candidate genes
-        method = "OmegaPlus"
-        f.write(f"""bedtools intersect -a {gff_file} -b {output_file} -wa \\
-| awk '{{$2="{method}"; print}}' \\
-> {output_file}.genes.gff
+            # Get candidate genes
+            f.write(f"""bedtools intersect -a {gff_file} -b {output_file} -wa \\
+    | awk '{{$2="{method}"; print}}' \\
+    > {output_file}.genes.gff
 
-cat {output_file}.genes.gff | awk -F'[;= ]' '{{for(i=1;i<=NF;i++) if($i=="Name") print $(i+1) "\t{method}"}}' \\
-> {output_file}.genes.txt
-""")
+    cat {output_file}.genes.gff | awk -F'[;= ]' '{{for(i=1;i<=NF;i++) if($i=="Name") print $(i+1) "\t{method}"}}' \\
+    > {output_file}.genes.txt
+    """)
 
-os.system(f"cd {sub_script_dir} && sbatch {merge_script}")
-print(wrap79(f"The following script has been submitted to slurm:"))
-print(wrap79(merge_script))
+    os.system(f"cd {sub_script_dir} && sbatch {merge_script}")
+    print(wrap79(f"The following script has been submitted to slurm:"))
+    print(wrap79(merge_script))
+
+ls = top_cut_off.copy()
+ls.append('')
+print(ls)
+for cutoff in ls:
+    merge_outliers('OmegaPlus', cutoff)    
+
+    
+
+# merge_script = os.path.join(sub_script_dir, f'{script_basename}.merge.sh')
+# with open(merge_script,'w') as f:
+#     f.write(f"#!/bin/bash\n")
+#     f.write(f"#SBATCH -e {os.path.basename(merge_script)}.%J.err\n")
+#     f.write(f"#SBATCH -e {os.path.basename(merge_script)}.%J.out\n")
+#     f.write(f"{load_bedtools}\n\n\n")
+#     for pop in populations:
+#         input_file = os.path.join(sub_output_dir, f'OmegaPlus.outliers.{pop}.bed')
+#         output_file = os.path.join(sub_output_dir, f'OmegaPlus.outliers.{pop}.merged.bed')
+#         gff_file = os.path.join(input_dir,gff_filename)
+#         f.write(f"bedtools merge -i {input_file} > {output_file}\n")
+
+#         # Get candidate genes
+#         method = "OmegaPlus"
+#         f.write(f"""bedtools intersect -a {gff_file} -b {output_file} -wa \\
+# | awk '{{$2="{method}"; print}}' \\
+# > {output_file}.genes.gff
+
+# cat {output_file}.genes.gff | awk -F'[;= ]' '{{for(i=1;i<=NF;i++) if($i=="Name") print $(i+1) "\t{method}"}}' \\
+# > {output_file}.genes.txt
+# """)
+
+# os.system(f"cd {sub_script_dir} && sbatch {merge_script}")
+# print(wrap79(f"The following script has been submitted to slurm:"))
+# print(wrap79(merge_script))
 
 
 
