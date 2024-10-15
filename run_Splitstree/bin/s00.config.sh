b904bf1d0c641164df8806c5f0616375737439db
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-10-15 15:35:30 +0200

    EFR

--- run_Splitstree/bin/s00.config.sh
+++ run_Splitstree/bin/s00.config.sh
@@ -1,3 +1,6 @@
 # INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch31.Asia.removeConflicts/pear_Dec2023.Asia.Combine_chr.geno20_maf005.anno.syno.thin8k"
 # INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch43.pear_Dec2023.Europe/pear_Dec2023.Europe.Combine_chr.geno20_maf005.anno.syno.thin8k"
-INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch42.full/pear_Dec2023.full.Combine_chr.geno20_maf005.anno.syno.thin8k"
+# INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch42.full/pear_Dec2023.full.Combine_chr.geno20_maf005.anno.syno.thin8k"
+# INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch46.noAdmix/pear_Dec2023.noAdmix.Combine_chr.geno20_maf005.anno.syno.thin8k"
+
+INPUT="/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch18.batch47.full_noAdmix_base_branch15_batch42_unlink/pear_Dec2023.full_noAdmix.Combine_chr.geno20_maf005.anno.syno.thin8k.noAdmix.geno20.plink"
