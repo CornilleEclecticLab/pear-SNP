a11f998380d36ee1e704484eb537fa7153693ee8
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-10-22 15:16:17 +0200

    run scrm

---
+++ run_scrm_simulator_refer_on_communis/bin/s00.download_scrm.sh
@@ -0,0 +1,4 @@
+wget https://github.com/scrm/scrm/releases/download/v1.7.4/scrm-x64-static
+
+# project link
+# https://github.com/scrm/scrm/
---
+++ run_scrm_simulator_refer_on_communis/bin/s00.load_scrm.sh
@@ -0,0 +1,1 @@
+export PATH="/shared/home/ynie/work/pear/run_scrm_simulator_refer_on_communis/bin/scrm.NOGIT.":$PATH
