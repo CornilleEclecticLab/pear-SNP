10d65199fb5d615c183d73bfbb8bb9341c31a5a9
NIE Yuqi <nieyuqi.cn@gmail.com>
2024-12-09 10:49:52 +0100

    adjust plot

--- run_Pixy_PiFstDxy/bin/s03.plot.R
+++ run_Pixy_PiFstDxy/bin/s03.plot.R
@@ -4,10 +4,10 @@
 
 ## INSTALL PACKAGES ----------------------------------------------
 ## install CRAN packages if needed
-pckgs <- c("ggplot2", "dplyr", "systemfonts", "ggforce", 
-           "ggdist", "ggbeeswarm", "devtools", "ggtext")
-new_pckgs <- pckgs[!(pckgs %in% installed.packages()[,"Package"])]
-if(length(new_pckgs)) install.packages(new_pckgs)
+# pckgs <- c("ggplot2", "dplyr", "systemfonts", "ggforce",
+#            "ggdist", "ggbeeswarm", "devtools", "ggtext")
+# new_pckgs <- pckgs[!(pckgs %in% installed.packages()[,"Package"])]
+# if(length(new_pckgs)) install.packages(new_pckgs)
 
 
 ## LOAD PACKAGES -------------------------------------------------
@@ -29,7 +29,7 @@ rm(list=ls())
 theme_set(
   theme_minimal(
     ## increase size of all text elements
-    base_size = 10, 
+    base_size = 6,
     ## set custom font family for all text elements
     # base_family = "Roboto Condensed"
     )
@@ -42,11 +42,11 @@ theme_update(
   ## remove all minor grid lines
   panel.grid.minor = element_blank(),
   ## remove axis titles element_blank(), or not element_text()
-  axis.title = element_text(), 
+  axis.title = element_text(size = 6),
   ## larger axis text for x
-  axis.text.x = element_text(size = 10),
+  axis.text.x = element_text(size = 6),
   ## add some white space around the plot
-  plot.margin = margin(rep(8, 4))
+  plot.margin = margin(0,0,0,0)
 )
 
 
@@ -59,12 +59,16 @@ setwd(script_path)
 # Function to convert Pixy output to long format
 pixy_to_long <- function(pixy_files) {
   pixy_df <- list()
-  
+
   for (i in 1:length(pixy_files)) {
+    if(!grepl("\\.txt$", pixy_files[i])){
+      next
+    }
+
     stat_file_type <- gsub(".*_|.txt", "", pixy_files[i])
-    
+
     df <- read_delim(pixy_files[i], delim = "\t")
-    
+
     if (stat_file_type == "pi") {
       df <- df %>%
         gather(-pop, -window_pos_1, -window_pos_2, -chromosome,
@@ -76,10 +80,10 @@ pixy_to_long <- function(pixy_files) {
         gather(-pop1, -pop2, -window_pos_1, -window_pos_2, -chromosome,
                key = "statistic", value = "value")
     }
-    
+
     pixy_df[[i]] <- df
   }
-  
+
   bind_rows(pixy_df) %>%
     arrange(pop1, pop2, chromosome, window_pos_1, statistic)
 }
@@ -107,7 +111,7 @@ pixy_df <- pixy_to_long(pixy_files)
 
 # Plot: Pi ---------------------------------------------  
 ## Got avg_pi data by windows
-pop_order = read.table("../input/s03.population_order.tree.txt", header=FALSE)$V1
+pop_order = read.table("../input/s03.population_order.pi_plot.txt", header=FALSE)$V1
 pi_data <- pixy_df %>% 
   filter(statistic == "avg_pi") %>%
   mutate(pop1 = factor(pop1, levels = pop_order))
@@ -134,12 +138,12 @@ comparisions_data <- pixy_df %>%
 
 sum_comparisions_data <- comparisions_data %>%
   group_by(pop1) %>% 
-  summarise(total_value = sum(value, na.rm=TRUE)) %>% 
+  summarise(total_value = sum(value, na.rm=TRUE)) %>%
   mutate(total_value = as.numeric(total_value))
 
 
 total_pi = sum_diffs_data %>% 
-  inner_join(sum_comparisions_data, by="pop1", suffix = c("_diff", "_comp")) %>% 
+  inner_join(sum_comparisions_data, by="pop1", suffix = c("_diff", "_comp")) %>%
   mutate (pi = total_value_diff / total_value_comp) %>% 
   mutate(pi = round(pi,6)) %>% 
   select(pop1, pi)
@@ -161,10 +165,59 @@ my_color <- ori_order_color[pop_order]
 
 ## Read number of individuals (N)
 Ns = read.table(
-  "../output/s03.calculate_Pi_Dxy_across_chromosome.pixy_bed_1based/Pi.txt", 
+  "../output/s03.calculate_Pi_Dxy_across_chromosome.pixy_bed_1based/Pi.txt",
   header=T)
 Ns <- Ns %>% select(pop, N)
 colnames(Ns) <- c("pop1", "N")
+
+# Box plot
+boxplot <- ggplot(pi_data, aes(x=pop1, y=value, color=factor(pop1))) +
+  geom_boxplot(
+    width = .4,
+    outlier.shape = NA
+  ) +
+
+  # geom_jitter(
+  #   size = .3,
+  #   alpha = .1,
+  #  # control randomness and range of jitter
+  #   position = position_jitter(seed = 0, width =.04)
+  # ) +
+
+  # Add text for N values
+  geom_text(
+    data = Ns,
+    aes(x = pop1, y = max(pi_data$value) * 0.75,
+        label = paste("italic(N) == ", N)),
+    position = position_dodge(width = 0.8),
+    parse = TRUE,
+    size = 2,
+    hjust = 0.6,
+    vjust = 0
+  ) +
+
+  # Add text for total pi
+  geom_text(
+    data = total_pi,
+    aes(x = pop1, y = pi + 0.01, 
+        label = paste("pi ==", pi)),
+    position = position_dodge(width = 0.8),
+    parse = TRUE,
+    size = 2,
+    hjust = 0.5,
+    vjust = 0
+  ) +
+
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 45, hjust = .8, color = my_color),
+        axis.text.y = element_text(size = 6)
+  )+
+
+  labs(x = "Populations", y = "Average Pi by Windows") +
+  scale_color_manual(values = my_color)+
+  scale_fill_manual(values = my_color) 
+
+boxplot
 
 
 # Raincloud plot
@@ -181,55 +234,54 @@ plot <- ggplot(pi_data, aes(x=pop1, y=value, color=factor(pop1))) +
     .width = 0,
     point_colour = NA 
   ) +
-  
+
   geom_boxplot(
     width = .15,
     outlier.shape = NA
   ) +
 
-  geom_jitter(
-    size = .3,
-    alpha = .1,
-    # control randomness and range of jitter
-    position = position_jitter(seed = 0, width =.04)
-  ) +
-  
+  # geom_jitter(
+  #   size = .3,
+  #   alpha = .1,
+  #  # control randomness and range of jitter
+  #   position = position_jitter(seed = 0, width =.04)
+  # ) +
+
   # Add text for N values
   geom_text(
     data = Ns,
-    aes(x = pop1, y = max(pi_data$value) * 0.75, 
+    aes(x = pop1, y = max(pi_data$value) * 0.75,
         label = paste("italic(N) == ", N)),
     position = position_dodge(width = 0.8),
     parse = TRUE,
-    size = 3,
+    size = 2,
     hjust = 0.5,
     vjust = 0
   ) +
-  
+
   # Add text for total pi
   geom_text(
     data = total_pi,
-    aes(x = pop1, y = pi + 0.01, 
+    aes(x = pop1, y = pi + 0.01,
         label = paste("pi ==", pi)),
     position = position_dodge(width = 0.8),
     parse = TRUE,
-    size = 3,
+    size = 2,
     hjust = 0.5,
     vjust = 0
   ) +
-  
+
   theme(legend.position = "none",
-        axis.text.x = element_text(angle = 60, hjust = .8, color = my_color),
-        axis.text.y = element_text(size = 10)
+        axis.text.x = element_text(angle = 45, hjust = .8, color = my_color),
+        axis.text.y = element_text(size = 6)
   )+
-  
+
   labs(x = "Populations", y = "Average Pi by Windows") +
   scale_color_manual(values = my_color)+
   scale_fill_manual(values = my_color) 
-
 plot
 
-ggsave("../output/pi_avg_boxplot.pdf", p, width = 5, height = 4)
+ggsave("../output/pi_avg_raincloud_plot.pdf", plot, width = 11, height = 6)
 
 
 # Statistic:  ---------------------------------------------  
@@ -277,144 +329,89 @@ myletters <- multcompLetters(mymat,compare="<=",threshold=0.01,Letters=letters)
 myletters_df <- data.frame(pop=names(myletters$Letters),letter = myletters$Letters )
 
 
-p_sig <- plot + geom_text(data = myletters_df, mapping=aes(label = letter, x =pop, y = 0.025 ), colour="black", size=4)
+p_sig <- plot + geom_text(data = myletters_df, mapping=aes(label = letter, x =pop, y = 0.025 ), colour="black", size=2)
 p_sig
-require(cowplot)
-
-means <- aggregate(value ~ pop1, data = pi_data, FUN = mean)
-sds <- aggregate(value ~ pop1, data = pi_data, FUN = sd)
-result <- data.frame(treatment = means$pop1, mean = means$value, sd = sds$value)
-
-# Box plot
-
-plot <- ggplot() +
-  geom_jitter(pi_data, mapping = aes(x = pop1, y = value), width = 0.2, color = "#DDDDDD", size = 1) +
-  geom_boxplot(pi_data, mapping = aes(x = pop1, y = value, fill = pop1), outlier.shape = NA) +
-  stat_summary(pi_data, mapping = aes(x = pop1, y = value, fill = pop1), fun = mean, geom = "point", shape = 23, size = 4) +
-  #scale_fill_manual(values = c("#CC79A7", "#E69F00", "#D55E00", "#0072B2", "#F0E442", "#56B4E9")) +
-  theme_classic() +
-  ylim(0, 0.03) +
-  theme(legend.position = "none",
-        axis.text.x = element_text(angle = 60, hjust = 1))+
-  labs(x = "Populations", y = "Average Pi") 
-plot
-
-# Save the plot
 
 
+ggsave("../output/pi_avg_raincloud_plot_with_sig.pdf", p_sig, width = 18, height = 5)
+
+ggsave("../output/pi_avg_raincloud_plot_with_sig.180x65mm.pdf", p_sig, width = 180, height = 65, units="mm")
 
 
+################################################################################
+# Seperate Western and Eastern groups
+Western <- c("comm_Dessert", "comm_Perry", "pyra", "cauc")
+pi_data <- pi_data %>% 
+  mutate(group = ifelse(pop1 %in% Western, "Western", "Eastern"))
+
+group_color <- c(Western="#DDD6ff", Eastern="#FFD3D3")
+
+# Perform significance tests within each group
+library(nortest)
+
+# Shapiro-Wilk normality test within each group
+### Result: all values < 1e-3, not normal distribution
+pi_data %>%
+  group_by(group, pop1) %>%
+  summarise(shapiro_p = shapiro.test(value)$p.value, .groups = "drop") %>%
+  print()
+
+# Anderson-Darling normality test within each group
+### Result: all values < 1e-3, not normal distribution
+pi_data %>%
+  group_by(group, pop1) %>%
+  summarise(ad_p = ad.test(value)$p.value, .groups = "drop") %>%
+  print()
+
+# Perform Wilcoxon significance tests within each group
+significance_results <- pi_data %>%
+  group_by(group) %>%
+  summarise(
+    wilcox_result = list(pairwise.wilcox.test(value, pop1, p.adjust.method = "holm")),
+    .groups = "drop"
+  )
+
+# Extract p-values and calculate significance letters for each group
+group_Western_pvalues <- significance_results$wilcox_result[[1]]$p.value
+group_Western_matrix <- tri.to.squ(group_Western_pvalues)
+group_Western_letters <- multcompLetters(group_Western_matrix, compare = "<=", threshold = 0.01, Letters = letters)
+
+group_Eastern_pvalues <- significance_results$wilcox_result[[2]]$p.value
+group_Eastern_matrix <- tri.to.squ(group_Eastern_pvalues)
+group_Eastern_letters <- multcompLetters(group_Eastern_matrix, compare = "<=", threshold = 0.01, Letters = letters)
+
+# Combine results into a dataframe
+letters_df <- bind_rows(
+  data.frame(pop = names(group_Western_letters$Letters), letter = group_Western_letters$Letters, group = "Western"),
+  data.frame(pop = names(group_Eastern_letters$Letters), letter = group_Eastern_letters$Letters, group = "Eastern")
+)
+
+split_index <- which(letters_df$pop == "pyri_JP")
+
+# Add significance labels to the plot
+p_sig_grouped <- plot +
+  geom_text(
+    data = letters_df,
+    aes(label = letter, x = pop, y = 0.025, color=group),
+    size = 2
+  )+
+  # Add split line
+  geom_vline(xintercept = split_index + 0.5, linetype = "dashed", color = "black", size = 0.7)
+p_sig_grouped
+
+# Save the updated plot
+ggsave("../output/pi_avg_raincloud_plot_with_sig_grouped_without_scatters.180x65mm.pdf", p_sig_grouped, width = 180, height = 65, units="mm")
 
 
-
-
-
-
-
-
-
-
-######>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-# Convert pixy output to long format
-pixy_to_long <- function(pixy_files){
-  pixy_df <- list()
-  
-  for(i in 1:length(pixy_files)){
-    stat_file_type <- gsub(".*_|.txt", "", pixy_files[i])
-    
-    if(stat_file_type == "pi"){
-      df <- read_delim(pixy_files[i], delim = "\t")
-      df <- df %>%
-        gather(-pop, -window_pos_1, -window_pos_2, -chromosome,
-               key = "statistic", value = "value") %>%
-        rename(pop1 = pop) %>%
-        mutate(pop2 = NA)
-      pixy_df[[i]] <- df
-    
-    } else{
-      df <- read_delim(pixy_files[i], delim = "\t")
-      df <- df %>%
-        gather(-pop1, -pop2, -window_pos_1, -window_pos_2, -chromosome,
-               key = "statistic", value = "value")
-      pixy_df[[i]] <- df
-    }
-  }
-  bind_rows(pixy_df) %>%
-    arrange(pop1, pop2, chromosome, window_pos_1, statistic)
-}
-
-pixy_folder <- "../output/s02.run_pixy_by_chromosome.mask_input"
-pixy_files <- list.files(pixy_folder, full.names = TRUE)
-pixy_df <- pixy_to_long(pixy_files)
-
-
-
-# Plot
-# Pi box plot
-
-
-
-
-
-# Single chromosome view
-# custom labeller for special characters in pi/dxy/fst
-pixy_labeller <- as_labeller(c(avg_pi = "pi",
-                             avg_dxy = "D[XY]",
-                             avg_wc_fst = "F[ST]"),
-                             default = label_parsed)
-
-# plotting summary statistics along a single chromosome
-pixy_df %>%
-  filter(chromosome == "GWHBAOS00000076") %>%
-  filter(statistic %in% c("avg_pi", "avg_dxy", "avg_wc_fst")) %>%
-  mutate(chr_position = ((window_pos_1 + window_pos_2)/2)/1000000) %>%
-  ggplot(aes(x = chr_position, y = value, color = statistic))+
-  geom_line(size = 0.25)+
-  facet_grid(statistic ~ .,
-             scales = "free_y", switch = "x", space = "free_x",
-             labeller = labeller(statistic = pixy_labeller,
-                                 value = label_value))+
-  xlab("Position on Chromosome (Mb)")+
-  ylab("Statistic Value")+
-  theme_bw()+
-  theme(panel.spacing = unit(0.1, "cm"),
-        strip.background = element_blank(),
-        strip.placement = "outside",
-        legend.position = "none")+
-  scale_x_continuous(expand = c(0, 0))+
-  scale_y_continuous(expand = c(0, 0))+
-  scale_color_brewer(palette = "Set1")
-
-
-# A genome-wide plot of summary statistics
-# create a custom labeller for special characters in pi/dxy/fst
-pixy_labeller <- as_labeller(c(avg_pi = "pi",
-                             avg_dxy = "D[XY]",
-                             avg_wc_fst = "F[ST]"),
-                             default = label_parsed)
-
-# plotting summary statistics across all chromosomes
-pixy_df %>%
-  mutate(chrom_color_group = case_when(as.numeric(chromosome) %% 2 != 0 ~ "even",
-                                 TRUE ~ "odd" )) %>%
-  mutate(chromosome = factor(chromosome, levels = c(1:17))) %>%
-  filter(statistic %in% c("avg_pi", "avg_dxy", "avg_wc_fst")) %>%
-  ggplot(aes(x = (window_pos_1 + window_pos_2)/2, y = value, color = chrom_color_group))+
-  geom_point(size = 0.5, alpha = 0.5, stroke = 0)+
-  facet_grid(statistic ~ chromosome,
-             scales = "free_y", switch = "x", space = "free_x",
-             labeller = labeller(statistic = pixy_labeller,
-                                 value = label_value))+
-  xlab("Chromsome")+
-  ylab("Statistic Value")+
-  scale_color_manual(values = c("grey50", "black"))+
-  theme_classic()+
-  theme(axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        panel.spacing = unit(0.1, "cm"),
-        strip.background = element_blank(),
-        strip.placement = "outside",
-        legend.position ="none")+
-  scale_x_continuous(expand = c(0, 0)) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0,NA))
+# Add significance labels to the boxplot
+boxplot_sig_grouped <- boxplot +
+  geom_text(
+    data = letters_df,
+    aes(label = letter, x = pop, y = 0.025, color=group),
+    size = 2
+  )+
+  # Add split line
+  geom_vline(xintercept = split_index + 0.5, linetype = "dashed", color = "black", size = 0.7)
+boxplot_sig_grouped
+# Save the updated plot
+ggsave("../output/pi_avg_box_plot_with_sig_grouped_without_scatters.180x65mm.pdf", boxplot_sig_grouped, width = 180, height = 65, units="mm")
