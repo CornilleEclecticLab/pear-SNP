9fbd09c949c0ae443f5d74774a7abe01661bab0d
NIE Yuqi <nieyuqi.cn@gmail.com>
2025-02-17 10:10:09 +0100

    update

--- run_OmegaPlus_refer_on_communis/bin/s00_config_summary_table.py
+++ run_OmegaPlus_refer_on_communis/bin/s00_config_summary_table.py
@@ -7,3 +7,5 @@ positive_selection_output_list = '../input/s08.summary_list.txt'
 chrID_map = '../input/PyrusCommunis_BartlettDHv2.0.chrID_map.txt'
 
 collinearity = '../../run_MCScanX/output/s05.parse_MCScanX/intersection.collinearity.txt'
+
+blast_annotation = '../../run_blast/output/s04.annotate_blast_result/PyrusCommunis_BartlettDHv2.0.pep.formatted.blastp.annotated.tsv'
--- run_OmegaPlus_refer_on_communis/bin/s08.summary_table.py
+++ run_OmegaPlus_refer_on_communis/bin/s08.summary_table.py
@@ -42,6 +42,8 @@ def wrap79(text, width=79):
 pos_genes = {}
 with open(positive_selection_output_list) as f:
     files = f.read().strip().split('\n')
+
+common_and_specific_pop_files = {}
 for file in files:
     with open(file) as f:
         for l in f:
@@ -59,13 +61,21 @@ for file in files:
             pos_genes[gene] = pos_genes.get(gene, {})   # pos_genes {gene:{}}
             pos_genes[gene][pop] = pos_genes[gene].get(pop, [])  # pos_genes {gene:{pop:[]}}
             if method in pos_genes[gene][pop]:
-                continue
+                print(f'Warning: {gene} {pop} {method} already in {positive_selection_output_list}')
                 #raise ValueError(f'Warning: {gene} {pop} {method} already in {positive_selection_output_list}')
             pos_genes[gene][pop].append(method)                  # pos_genes {gene:
                                                                  #                {pop1:[method1, method2, ...]}
                                                                  #                 pop2:[method1, ...]}
                                                                  # }
-
+for pop in set(pop for pop_methods in pos_genes.values() for pop in pop_methods):
+    common_and_specific_pop_files[pop] = open(os.path.join(
+        sub_output_dir, f'common_and_specific.positive_selection_genes.{pop}.txt'), 'w')
+    # Gene, method, pop
+    for gene, pop_methods in pos_genes.items():
+        for pop_key in pop_methods.keys():
+            if pop_key == pop:
+                common_and_specific_pop_files[pop].write(gene+'\t'+pop_key+'\t'+','.join(pop_methods[pop_key])+'\n')
+    
 # Load chr ID and accession map
 chr_ID_accession_map = {}
 if os.path.exists(chrID_map):
@@ -87,8 +97,8 @@ with open(gff_ori) as f:
             continue
         start = splits[3]
         end = splits[4]
-        attributions = splits[8].split(';')
-        dic = {i.split('=')[0]:i.split('=')[1] for i in attributions}
+        attributions = splits[8].replace(" ",'').split(';')
+        dic = {i.split('=')[0]:i.split('=')[1] for i in attributions if i != ''}
         if 'Name' in dic:
             name = dic['Name']
         elif 'Accession' in dic:
@@ -149,19 +159,26 @@ with open(eggNOG) as f:
                             'go':go}
 
 
+# Laod blast annotation
+blast_annotation_dic = {}
+with open(blast_annotation) as f:
+    lines = f.readlines() 
+    blast_header = '\t'.join(lines[0].strip().split('\t')[1:])
+    blast_annotation_dic = {line.strip().split('\t')[0]:'\t'.join(line.strip().split('\t')[1:]) for line in lines[1:]}
+
+
 # Write summary table
 ## Expected columns: Gene_ID, Chr, Start, End, Detected_methods, common_vs_specific, Detedted_populations, Gene_name Gene_description
 with open(os.path.join(sub_output_dir,'positive_selection_summary_table.tsv'),'w') as f:
     header = '\t'.join(['Gene_ID', 'Chr_accession', 'Chr_ID', 'Start', 'End', 'Detected_methods',
                         'Collinarity_with_the_other_reference_genome', 'Common_vs_specific_in_population',
-                        'Detected_populations', 'Gene_name', 'PFAM', 'Gene_description',
+                        'Detected_populations', 'Gene_name', 'PFAM', 'Gene_description', blast_header,
                         'GO', 'KEGG_Pathway', 'Reference_articles'])+'\n'
     f.write(header)
     
     specific_pop_files = {}
     for pop in set(pop for pop_methods in pos_genes.values() for pop in pop_methods):
-        specific_pop_files[pop] = open(os.path.join(sub_output_dir, f'specific.positive_selection_genes.{pop}.txt'), 'w')
-        specific_pop_files[pop].write(header)
+        specific_pop_files[pop] = open(os.path.join(sub_output_dir, f'specific.positive_selection_genes_list.{pop}.txt'), 'w')
 
     specific_summary = open(os.path.join(sub_output_dir, 'specific.positive_selection_genes.summary.txt'), 'w')
     specific_summary.write(header)
@@ -191,10 +208,12 @@ with open(os.path.join(sub_output_dir,'positive_selection_summary_table.tsv'),'w
         # remove duplicated methods
         detected_methods = ','.join(list(set(detected_methods.split(','))))
 
-        common_vs_specific = (
-            'common' if len(pos_genes[gene]) > 1
-            else 'specific' if  len(pos_genes[gene]) ==1
-            else ValueError(f'No population detected for gene {gene}') )
+        if len(pos_genes[gene]) > 1:
+            common_vs_specific = 'common'
+        elif len(pos_genes[gene]) == 1:
+            common_vs_specific = 'specific'
+        else:
+            raise ValueError(f'No population detected for gene {gene}')
         
         detected_populations = ','.join(list(pos_genes[gene].keys()))
         
@@ -211,15 +230,23 @@ with open(os.path.join(sub_output_dir,'positive_selection_summary_table.tsv'),'w
         
         gene_description = eggNOG_dic.get(gene,{}).get('description','NA')
         
-        write_line = f'{gene}\t{chr_accession}\t{chr}\t{start}\t{end}\t{detected_methods}\t{collinearity}\t{common_vs_specific}\t{detected_populations}\t{gene_name}\t{pfam}\t{gene_description}\t{go}\t{kegg_pathways}\t\n'
+        blast_annotation = blast_annotation_dic.get(gene, str('NA\t'*len(blast_header.split('\t'))).strip())
+        
+        write_line = f'{gene}\t{chr_accession}\t{chr}\t{start}\t{end}\t{detected_methods}\t{collinearity}\t{common_vs_specific}\t{detected_populations}\t{gene_name}\t{pfam}\t{gene_description}\t{blast_annotation}\t{go}\t{kegg_pathways}\t\n'
 
         f.write(write_line)
-        
-        if common_vs_specific == 'specific':
-            specific_pop_files[pop].write(write_line)
-            specific_summary.write(write_line)
-            
 
+        if common_vs_specific == 'specific':
+            specific_pop = list(pos_genes[gene].keys())[0]
+            specific_pop_files[specific_pop].write(gene+'\n')
+            specific_summary.write(write_line)
+
+# Close files
+for pop_file in common_and_specific_pop_files.values():
+    pop_file.close()
+for pop_file in specific_pop_files.values():
+    pop_file.close()
+specific_summary.close()
 
 
 end_time = datetime.datetime.now()
