1137cf90f156581a7243aae9c9c793a0fe351e11
NIE Yuqi <nieyuqi.cn@gmail.com>
2025-07-28 16:55:27 +0200

    update input filter and output name

--- run_OmegaPlus/bin/s00_config.py
+++ run_OmegaPlus/bin/s00_config.py
@@ -1,4 +1,21 @@
 batch='pear.Dec2023'
-input_merged_vcf_path='/shared/ifbstor1/projects/pear_snp3/pear/run_Population_genetic_filter/output/branch15.batch30.noclone.removeConflictss/pear_Dec2023.noClone.Combine_chr.geno20.small.vcf.gz'
-chromosomes_list_filename = 's02.chr_list.txt'
-individual_pop_map_filename = 's02.sample_tab_population.txt'
+
+variant_vcf_list_filename = 's01.variant_vcf_list.txt'
+
+chromosomes_list_filename = 's01.chr_list.txt'
+
+individual_pop_map_filename = 's01.sample_tab_population.txt'
+
+omega_plus_output_dir = 's03.generate_run_OmegaPlus'
+
+cutoff_files_dir = '../output/s03.find_FPR_cutoff_OmegaPlus'
+
+cutoff_files_suffix = 'OmegaPlus.FPR1pct.cutoff.txt'
+
+load_bedtools = "module load bedtools/2.30.0"
+
+gff_filename = "GWHBAOS00000000.chr_list.gene.slop1kb.gff"
+
+# cds_fasta_filename = ""
+
+mask_pass_filename='GWHBAOS00000000.chr_list.fasta_centromere_range.txt.genmap_pass_subtract_centier.bed'
--- run_OmegaPlus/bin/s03.generate_run_OmegaPlus.py
+++ run_OmegaPlus/bin/s03.generate_run_OmegaPlus.py
@@ -20,6 +20,10 @@
 #          v1.3.0 2024-11-03
 #     1. Use the masked vcf as input. 
 
+#          v1.4.0 2025-01-17
+#     1. Use back the unmased vcf file
+
+
 import datetime
 import sys
 import textwrap
@@ -35,7 +39,7 @@ print(f'{" Start ":=^79}')
 
 
 
-version = "1.3.0"
+version = "1.4.0"
 script_basename = os.path.splitext(os.path.basename(sys.argv[0]))[0]
 script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 bin_dir = script_path
@@ -119,7 +123,7 @@ source {bin_dir}/s00.load_OmegaPlus.sh
 # Run OmegaPlus
 OmegaPlus-M \\
     -name {pop_chr} \\
-    -input {vcf_prefix}.masked.vcf \\
+    -input {vcf_prefix}.vcf \\
     -grid {grid_num} \\
     -minwin 10000 \\
     -maxwin 200000 \\
@@ -127,6 +131,39 @@ OmegaPlus-M \\
     -minsnps 5 \\
     -threads 4 \\
     -seed {random_seed}
+
+# OmegaPlus-M \\
+#     -name {pop_chr}.minwin5K_maxwin100K \\
+#     -input {vcf_prefix}.vcf \\
+#     -grid {grid_num} \\
+#     -minwin 5000 \\
+#     -maxwin 100000 \\
+#     -all \\
+#     -minsnps 5 \\
+#     -threads 4 \\
+#     -seed {random_seed}
+
+# OmegaPlus-M \\
+#     -name {pop_chr}.minwin1K_maxwin100K \\
+#     -input {vcf_prefix}.vcf \\
+#     -grid {grid_num} \\
+#     -minwin 1000 \\
+#     -maxwin 100000 \\
+#     -all \\
+#     -minsnps 5 \\
+#     -threads 4 \\
+#     -seed {random_seed}
+
+# OmegaPlus-M \\
+#     -name {pop_chr}.minwin8K_maxwin80K \\
+#     -input {vcf_prefix}.vcf \\
+#     -grid {grid_num} \\
+#     -minwin 8000 \\
+#     -maxwin 80000 \\
+#     -all \\
+#     -minsnps 5 \\
+#     -threads 4 \\
+#     -seed {random_seed}
 
 mv OmegaPlus_Info.{pop_chr} {output_dir}
 mv OmegaPlus_Report.{pop_chr} {output_dir} 
