2053ad5032cb612d6ba9a2a1c576e086001c9ce8
NIE Yuqi <nieyuqi.cn@gmail.com>
2025-09-02 15:56:51 +0200

    run FastEPRR

---
+++ run_FastEPRR_refer_on_pyrifolia/bin/s00_config.py
@@ -0,0 +1,19 @@
+chrID_number_txt = 'chrID_number.txt'
+
+s01_dir = 's01.get_phased_vcf_by_pop_by_chr'
+
+s02_dir = 's02.rename_chr_ID'
+
+s03_dir = 's03.generate_run_FastEPRR_step1'
+
+s04_dir = 's04.generate_run_FastEPRR_step2'
+
+s05_dir = 's05.generate_run_FastEPRR_step3'
+
+s06_dir = 's06.generate_run_FastEPRR_step4'
+
+sample_population_txt = 's01.sample_tab_population.txt'
+
+# The windows and step length for FastEPRR_VCF_step1() and FastEPRR_VAR()
+winLength = 100000
+stepLength = 50000
